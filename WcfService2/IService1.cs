1608f6846f350ef5678241a6b732097063720135
rogersgt <rogersgt819@gmail.com>
2016-12-22 13:33:21 -0500

    initializing repo

---
+++ WcfService2/IService1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+
+        [OperationContract]
+        string GetUsername(int value);
+
+        [OperationContract]
+        UserType AddUser(bool admin, string username, string password, string firstname, string lastname);
+
+        [OperationContract]
+        bool Authenticate(string username, string password);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class UserType
+    {
+        bool admin = false;
+        string username,
+               password,
+               firstname,
+               lastname;
+
+        [DataMember]
+        public string Username
+        {
+            get { return username; }
+            set { username = value; }
+        }
+
+        [DataMember]
+        public string Password
+        {
+            get { return password; }
+            set { password = value; }
+        }
+
+        [DataMember]
+        public string Firstname
+        {
+            get { return firstname; }
+            set { firstname = value; }
+        }
+
+        [DataMember]
+        public string Lastname
+        {
+            get { return lastname; }
+            set { lastname = value; }
+        }
+
+        [DataMember]
+        public bool Admin
+        {
+            get { return admin; }
+        }
+    }
+}
---
+++ WcfService2/Service1.svc.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+using System.Xml.Linq;
+using System.Resources;
+using System.Web;
+using System.IO;
+
+namespace SimpleService
+{
+    public class Service1 : IService1
+    {
+        private XElement doc;
+
+        private void FillXML()
+        {
+            string xmlData = Properties.Resources.users;
+            doc = XElement.Parse(xmlData);       
+        }
+
+        public string GetUsername(int ID)
+        {
+            FillXML();
+            foreach (XElement el in doc.Elements())
+            {
+                int id = Int32.Parse(el.Element("ID").Value);
+                if (ID == id)
+                {
+                    return el.Element("username").Value;
+                }
+            }
+            return "ID does not match any existing users";
+        }
+
+        private void SaveUser (bool admin, UserType user)
+        {
+            FillXML();
+            XElement el = new XElement("user", new XAttribute("admin", admin));
+            el.Add(new XElement("username", user.Username));
+            el.Add(new XElement("password", user.Password));
+            el.Add(new XElement("firstname", user.Firstname));
+            el.Add(new XElement("lastname", user.Lastname));
+
+            doc.Add(el);
+
+            string path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "data", "users.xml");
+            doc.Save(path);
+        }
+
+        public UserType AddUser(bool admin, string username, string password, string firstname, string lastname)
+        {
+            UserType user = new UserType();
+            user.Firstname = firstname;
+            user.Lastname = lastname;
+            user.Password = password;
+
+            SaveUser(admin, user);
+            
+            return user;
+        }
+
+        public bool Authenticate(string username, string password)
+        {
+            FillXML();
+
+            foreach (XElement el in doc.Elements())
+            {
+                if ((el.Element("username").Value == username) && (el.Element("password").Value == password))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
+}
